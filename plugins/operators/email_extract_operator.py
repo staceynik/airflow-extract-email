480d63e43388e3035391af87473a6900eba2f201
staceynik <staceynik@mail.ru>
2023-08-24 18:22:30 +0200

    add database

--- plugins/operators/email_extract_operator.py
+++ plugins/operators/email_extract_operator.py
@@ -1,6 +1,7 @@
 from airflow.models import BaseOperator
 from airflow.utils.decorators import apply_defaults
 import requests
+import psycopg2
 
 class EmailExtractOperator(BaseOperator):
     @apply_defaults
@@ -14,4 +15,33 @@ class EmailExtractOperator(BaseOperator):
         filtered_comments = [comment for comment in comments if comment['email'].endswith('us')]
 
         self.log.info("Extracted %d comments with emails ending with 'us'", len(filtered_comments))
+
+        connection = psycopg2.connect(
+        host="postgres.cxubmpofrvqu.us-east-1.rds.amazonaws.com",
+        port="5432",
+        user="postgres",
+        password="postgres",
+        dbname="postgres"
+    )
+
+    cursor = connection.cursor()
+
+
+    cursor.execute("""
+        CREATE TABLE IF NOT EXISTS extracted_emails (
+            id SERIAL PRIMARY KEY,
+            email TEXT NOT NULL,
+            comment TEXT
+        )
+    """)
+
+    for comment in filtered_comments:
+        cursor.execute(
+            "INSERT INTO extracted_emails (email, comment) VALUES (%s, %s)",
+            (comment['email'], comment['body'])
+        )
+
+    connection.commit()
+    cursor.close()
+    connection.close()
 
